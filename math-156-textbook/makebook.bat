cf19846ef79e4c2681ba04f1d753c69b704f9d44
diemerNIC <jason.diemer@nic.bc.ca>
2021-06-05 11:30:01 -0700

    clean up

--- math-156-textbook/makebook.bat
+++ math-156-textbook/makebook.bat
@@ -1,3 +1,3 @@
 python c:/users/diemer/documents/pretext/mathbook/pretext/pretext -vv -c latex-image -f svg -d ./output/html/images ./source/main.ptx
 
-xsltproc --output .\output\html\ --stringparam exercise.divisional.solution "no" --stringparam publisher publisher.ptx C:\users\diemer\Documents\PreTeXt\mathbook\xsl\pretext-html.xsl .\source\main.ptx
+xsltproc --output .\output\html\ --stringparam exercise.divisional.solution "no" --stringparam publisher publisher.ptx ~\Documents\mathbook\xsl\pretext-html.xsl .\source\main.ptx
