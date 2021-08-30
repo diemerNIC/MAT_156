6dd3ea168103cca126008bf4020051b377792c0f
diemerNIC <jason.diemer@nic.bc.ca>
2021-08-30 15:41:34 -0700

    through 3.3 exercises (no solutions yet)

--- math-156-textbook/makebook.bat
+++
@@ -1,3 +0,0 @@
-python c:/users/diemer/documents/pretext/mathbook/pretext/pretext -vv -c latex-image -f svg -d ./output/html/images ./source/main.ptx
-
-xsltproc --output .\output\html\ --stringparam exercise.divisional.solution "no" --stringparam publisher publisher.ptx ~\Documents\mathbook\xsl\pretext-html.xsl .\source\main.ptx
